4fe55b03d13f4106f65f981a2eebcca3438aaf4b
Ricardo Vera <ricardovera71@gmail.com>
2023-03-17 11:55:50 -0500

    fix(issues)

--- ms_ops/settings.py
+++ ms_ops/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # USE OF CORS
 CORS_ALLOWED_ORIGINS = [
