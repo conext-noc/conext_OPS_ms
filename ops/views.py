8d933eeb242c80be7a7a9c400f9cb4b8f207685a
ricardovera76 <ricardovera71@gmail.com>
2023-03-17 12:50:30 -0500

    fix(get response)

--- ops/views.py
+++ ops/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework import generics
+from django.http import HttpResponse
 
 from ops.scripts.OP import operate
 
 
 class OPS(generics.GenericAPIView):
   def get(self,req):
-    return Response({"message":"MS_RUNNING"})
+    return HttpResponse("ms_running",status=200)
   def post(self,req):
     data = req.data
     res = []
