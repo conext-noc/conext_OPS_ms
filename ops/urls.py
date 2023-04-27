618fbd1338f0968fb12bae0147d01ac33f834e36
Ricardo Vera <ricardovera71@gmail.com>
2023-04-27 14:53:40 -0500

    add(check & sec): added check endpoint and security to main endpoint

--- ops/urls.py
+++ ops/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OPS
+from .views import OPS, CHECK
 
 urlpatterns = [
     path("", OPS.as_view()),
+    path("check/", CHECK.as_view()),
 ]
--- ops/views.py
+++ ops/views.py
@@ -1,16 +1,29 @@
 from rest_framework.response import Response
 from rest_framework import generics
 from django.http import HttpResponse
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 from ops.scripts.OP import operate
 
 
+class CHECK(generics.GenericAPIView):
+  def get(self, req):
+    status_code = 200
+    response_text = "ms_running"
+    return HttpResponse(response_text, status=status_code)
+
+
 class OPS(generics.GenericAPIView):
-  def get(self,req):
-    return HttpResponse("ms_running",status=200)
   def post(self,req):
-    data = req.data
-    res = []
-    for client in data["clients"]:
-      res.append(operate(client))
-    return Response({"message":"OK", "data":res})
+    if req.META['HTTP_CONEXT_KEY'] == os.environ["CONEXT_KEY"]:
+      status_code = 200
+      data = req.data
+      res = []
+      for client in data["clients"]:
+        res.append(operate(client))
+      return Response({"message":"OK", "data":res})
+    else:
+      return HttpResponse("Bad Request to server", status=400)
