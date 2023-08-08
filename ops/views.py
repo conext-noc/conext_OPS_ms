ffea0ea2916d1cdc890e6760401a5109a6c44ce5
Ricardo Vera <ricardovera71@gmail.com>
2023-08-08 00:16:09 -0500

    refactor(dry)

--- ops/views.py
+++ ops/views.py
@@ -1,6 +1,5 @@
 import json
 import os
-from rest_framework.response import Response
 from rest_framework import generics
 from django.http import HttpResponse, StreamingHttpResponse
 from dotenv import load_dotenv
@@ -10,15 +9,14 @@ from ops.scripts.OX import client_operate
 load_dotenv()
 
 
-class OPS(generics.GenericAPIView):
-    def data_generator(self, data):
-        for client in data["clients"]:
-            res = client_operate(
-                client
-            )  # You can adjust the number of iterations as needed
-            data = {"message": "OK", "error": False, "data": [res]}
-            yield json.dumps(data) + "\n"
+def data_generator(data):
+    for client in data["clients"]:
+        res = client_operate(client)
+        data = {"message": "OK", "error": False, "data": [res]}
+        yield json.dumps(data) + "\n"
 
+
+class OPS(generics.GenericAPIView):
     def get(self, _):
         return HttpResponse("ms_running", status=200)
 
@@ -26,7 +24,7 @@ class OPS(generics.GenericAPIView):
         if req.META["HTTP_CONEXT_KEY"] == os.environ["CONEXT_KEY"]:
             data = req.data
             response = StreamingHttpResponse(
-                self.data_generator(data), content_type="application/json"
+                data_generator(data), content_type="application/json"
             )
             return response
 
@@ -37,9 +35,8 @@ class OPSDashboard(generics.GenericAPIView):
     def post(self, req):
         data = req.data
         if data["API_KEY"] == os.environ["API_KEY"]:
-            res = []
-            for client in data["clients"]:
-                res.append(client_operate(client))
-            return Response({"message": "OK", "error": False, "data": res})
-
+            response = StreamingHttpResponse(
+                data_generator(data), content_type="application/json"
+            )
+            return response
         return HttpResponse("Bad Request to server", status=500)
