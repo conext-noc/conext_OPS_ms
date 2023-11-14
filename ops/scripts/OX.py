15e0fe8327c01ac2d0bfae0a8c6ee45ea3347e2c
CesarSanch1998 <cs96548@gmail.com>
2023-11-13 20:48:33 -0400

    fix(filtered_by_olt)

--- ops/scripts/OX.py
+++ ops/scripts/OX.py
@@ -10,34 +10,35 @@ def client_operate(data):
     result = "Reactivado" if "R" in action else "Suspendido"
 
     payload["lookup_type"] = "C"
-    payload["lookup_value"] = data["contract"] + f'_{data["olt"]}'
+    payload["lookup_value"] = data["contract"]
     req = db_request(endpoints["get_client"], payload)
 
-    if req["data"] is None:
-        return {
-            "message": "The required OLT & ONT does not exists",
-            "contract": data["contract"],
-        }
+    return req
+    # if req["data"] is None:
+    #     return {
+    #         "message": "The required OLT & ONT does not exists",
+    #         "contract": data["contract"],
+    #     }
 
-    client = req["data"]
-    (command, quit_ssh) = ssh(olt_devices[str(client["olt"])])
-    command(f'interface gpon {client["frame"]}/{client["slot"]}')
-    command(f'ont {operation} {client["port"]} {client["onu_id"]}')
+    # client = req["data"]
+    # (command, quit_ssh) = ssh(olt_devices[str(client["olt"])])
+    # command(f'interface gpon {client["frame"]}/{client["slot"]}')
+    # command(f'ont {operation} {client["port"]} {client["onu_id"]}')
 
-    payload["change_field"] = "OX"
-    payload["new_values"] = {"state": resulted_operation}
-    req = db_request(endpoints["update_client"], payload)
-    message = f'Cliente {client["name_1"]} {client["name_2"]} {client["contract"]} ha sido {result}'
-    quit_ssh()
+    # payload["change_field"] = "OX"
+    # payload["new_values"] = {"state": resulted_operation}
+    # req = db_request(endpoints["update_client"], payload)
+    # message = f'Cliente {client["name_1"]} {client["name_2"]} {client["contract"]} ha sido {result}'
+    # quit_ssh()
 
-    return {
-        "message": message,
-        "name": f'{client["name_1"]} {client["name_2"]} {client["contract"]}',
-        "fspi": client["fspi"],
-        "olt": client["olt"],
-        "frame": client["frame"],
-        "slot": client["slot"],
-        "port": client["port"],
-        "onu_id": client["onu_id"],
-        "state": resulted_operation,
-    }
+    # return {
+    #     "message": message,
+    #     "name": f'{client["name_1"]} {client["name_2"]} {client["contract"]}',
+    #     "fspi": client["fspi"],
+    #     "olt": client["olt"],
+    #     "frame": client["frame"],
+    #     "slot": client["slot"],
+    #     "port": client["port"],
+    #     "onu_id": client["onu_id"],
+    #     "state": resulted_operation,
+    # }
